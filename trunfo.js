4243bc699ba46c3c1e4f8c50cf172e015e0ac110
PinhoOn <pinhoonjob@gmail.com>
2021-04-01 14:25:02 -0300

    stats add

--- trunfo.js
+++ trunfo.js
@@ -1,57 +1,72 @@
 var cardMathDef = {
-  name: " Matheus Observador",
   image: "https://i.imgur.com/dLqM8zB.png",
+    stats:{
+  name: " Matheus Observador",
   attack: "0, pois ele observa",
   defense: "90, pois ele tudo vê",
-  magic: "0, apenas um observador",
-
+  magic: "0, apenas um observador"
+    }
 }
+
 var cardMathAtack = {
-  name:"Matheus Putaço",
-  image: "https://i.imgur.com/A8sMNLB.png",
-  attack:"90, pois ele ta puto carai",
-  defense:"0, ele só quer bater em geral",
-  magic:"50, o negocio é socão",
+  image:"https://i.imgur.com/A8sMNLB.png",
+  stats:{
+    name:"Matheus Putaço",
+    attack:"90, pois ele ta puto carai",
+    defense:"0, ele só quer bater em geral",
+    magic:"50, o negocio é socão"
+  }
 }
 
 var cardGabriel = {
-  name: "Gabriel Pinho",
   image: "https://i.imgur.com/jYSdOCt.png",
-  attack:"100000, Gabriel Pinho é implacavel",
-  defense:"100000, Achas que pode me ferir?",
-  magic:"100000, O proprio Bruxão",
+  stats:{
+    name: "Gabriel Pinho",
+    attack:"100000, Gabriel Pinho é implacavel",
+    defense:"100000, Achas que pode me ferir?",
+    magic:"100000, O proprio Bruxão",
+  
+}
 }
 
 var cardPinhoGent = {
-  name: "Pinho Gentleman",
   image: "https://i.imgur.com/YhWC8px.png",
-  attack:"0, Concede a vitoria ao oponente",
-  defense:"0, Deixa o oponente ganhar",
-  magic:"0, Cordialmente oferece a vitoria",
+  stats:{
+    name:"Pinho Gentleman",
+    attack:"0, Concede a vitoria ao oponente",
+    defense:"0, Deixa o oponente ganhar",
+    magic:"0, Cordialmente oferece a vitoria"
+  }
 }
 
 var cardPinhoAtack = {
-  name: "Pinho SaiaJeans",
   image: "https://i.imgur.com/GXX4hZk.png",
-  attack:"50, um iniciante na arte do duelo",
-  defense:"20, mó magrelo",
-  magic:"30, solta no maximo um peidinho",
+  stats:{
+    name: "Pinho SaiaJeans",
+    attack:"50, um iniciante na arte do duelo",
+    defense:"20, mó magrelo",
+    magic:"30, solta no maximo um peidinho"
+  }
 }
 
 var cardGuizoPokas = {
-  name: "Guizo Pokas Ideia",
   image: "https://i.imgur.com/aVRxxRY.png",
-  attack:"0, ele fica imovel",
-  defense:"0, menino fragil",
-  magic:"100, te mata com o olhar",
+    stats:{
+    name: "Guizo Pokas Ideia",
+    attack:"0, ele fica imovel",
+    defense:"0, menino fragil",
+    magic:"100, te mata com o olhar"
+  }
 }
 
 var cardGuizoReza = {
-  name: "Avemaria cheia de Guizo",
   image: "https://i.imgur.com/66qIh7C.png",
-  attack:"0, pois ele pede para ganhar",
-  defense:"0, ele só quer ganhar",
-  magic:"0, deixa ele ganhar mano",
+  stats:{
+    name: "Avemaria cheia de Guizo",
+    attack:"0, pois ele pede para ganhar",
+    defense:"0, ele só quer ganhar",
+    magic:"0, deixa ele ganhar mano"
+  }
 }
 var cardPinho
 var numberCardPlayer
@@ -68,9 +83,22 @@ function randomCard(){
 
   cardPlayer = cards[numberCardPlayer]
 
-var imageCardPlayer
-
-function imageCardPlayer(){
+function imageCardPinho(){
   document.getElementById("my__card").style.backgroundImage = `url(${cardPinho.image})`
 }
+imageCardPinho()
+
+function imageCardPlayer(){
+  document.getElementById("your__card").style.backgroundImage = `url(${cardPlayer.image})`
+}
+imageCardPlayer()
+
+function statsCardPlayer(){
+  var statsCardPlayer = document.getElementById("your--card--stats")
+  for (var stat in cardPlayer.stats)
+  var p = `<p> ${cardPlayer.stats} </p>`
+  statsCardPlayer.innerHTML = p
+}
+statsCardPlayer()
+
 }
